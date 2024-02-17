7e398b80f17cad0017b1ef3a9494f10adef36092
José Batista <batistdev@gmail.com>
2024-02-17 18:41:44 -0300

    Initial commit

---
+++ SportsStore/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
+namespace SportsStore.Controllers
+{
+	public class HomeController : Controller
+	{
+		private IStoreRepository repository;
+
+		public HomeController(IStoreRepository repository)
+		{
+			this.repository = repository;
+		}
+
+		public IActionResult Index()  => View(repository.Products);
+
+
+	}
+}
---
+++ SportsStore/Models/SeedData.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SportsStore.Models
+{
+	public static class SeedData
+	{
+		public static void EnsurePopulated(IApplicationBuilder app)
+		{
+			StoreDbContext context = app.ApplicationServices
+				.CreateScope().ServiceProvider.GetService<StoreDbContext>();
+
+			if (context.Database.GetPendingMigrations().Any())
+			{
+				context.Database.Migrate();
+			}
+
+			if (!context.Products.Any())
+			{
+				context.AddRange(
+					new Product
+					{
+						Name = "Kayak",
+						Description = "A boat for an person",
+						Category = "Watersports",
+						Price = 275
+					},
+
+					new Product
+					{
+						Name = "Lifejacket",
+						Description = "Protective and fashionable",
+						Category = "Watersports",
+						Price = 48.95M
+					}
+
+				);
+
+				context.SaveChanges();
+			}
+		}
+	}
+}
